e7549746235d6a978654cfcaea55fd705ce6579a
0142 <leksindmitri@gmail.com>
2024-01-31 18:32:26 +0400

    Add test alg for my wife

---
+++ wife.py
@@ -0,0 +1,39 @@
+list_1 = [
+    "topic.one-0-1",
+    "topic.one-1-1",
+    "topic.one-3-1",
+    "topic.one-2-1",
+    "topic.two-0-2"
+]
+
+list_2 = [
+    "topic.one-1-2",
+    "topic.one-2-2",
+    "topic.one-3-3",
+    "topic.one-2-2",
+    "topic.two-0-1"
+]
+
+list_3 = [
+    "topic.one-0-3",
+    "topic.two-0-2"
+    
+]
+
+list_4 = [
+    "topic.one-0-2",
+    "topic.one-2-3",
+    "topic.one-2-3",
+    "topic.one-3-2",
+    "topic.one-2-2",
+    "topic.two-0-3"
+]
+
+list_5 = [
+    "topic.one-0-2",
+    "topic.one-1-3",
+    "topic.one-2-3",
+    "topic.one-3-3",
+    "topic.one-2-2",
+    "topic.two-0-1"
+]
