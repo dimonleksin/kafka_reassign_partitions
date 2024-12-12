eae74ec5233c15217efc9b29b594a46ecd5ad2f6
leksindmitri <leksindmitri@gmail.com>
2024-12-12 22:17:20 +0400

    Changes to be committed: 	modified:   .github/workflows/go.yml 	modified:   .gitignore 	modified:   LICENSE 	modified:   README.md 	modified:   actions/move.go 	modified:   actions/rb.go 	modified:   go.mod 	modified:   go.sum 	modified:   pkg/clearZero.go 	modified:   pkg/describeTopic.go 	modified:   pkg/help.go 	modified:   pkg/rebalance.go 	modified:   pkg/scram_client.go 	modified:   pkg/search.go 	modified:   pkg/setting.go 	modified:   pkg/structs.go 	modified:   pkg/stuff.go 	modified:   pkg/version.go 	modified:   reasign_partitions.go 	modified:   tmp_text add sync moving topics

--- pkg/help.go
+++ pkg/help.go
@@ -14,7 +14,7 @@ func PrintHelp() {
 		"\tif u set password without --user, this call panic\n\n" +
 		"--mechanism [string] scram-sha-256 or scram-sha-512\n" +
 		"\tdefining only if you use auth with loggin and password\n\n" +
-		"--tls define if need uth TLS\n\n" +
+		"--tls define if need use TLS\n\n" +
 		"--ca [string] path to CA file\n\n" +
 		"--cert [string] path to cert file\n" +
 		"\tif you define --cert, dont forget defined --key\n\n" +
@@ -22,6 +22,8 @@ func PrintHelp() {
 		"--to [int] set brokers ids for desctination brokers (sep ','). For example 1,2,3\n\n" +
 		"--from [int] set source broker id\n\n" +
 		"--treads [int] seted number of treads for reassign\n\n" +
+		"--sync sync/async work with topic. If true - krpg wait when all replicas for partition moved in desired state\n" +
+		"\tif --treads > 1 then anyone thed wait mine topic\n\n" +
 		"--api-version set version of brokers in format 2.1.0 (default)\n\n" +
 		"--version print version from this build\n\n" +
 		"-h or --help for print this help"
